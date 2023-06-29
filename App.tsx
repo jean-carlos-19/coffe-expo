3a50042817be0c6f96dcbfc603df989d972ebfc2
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-29 16:56:02 -0500

    Modificado App.tsx: Imporntado el componente Home Al enrutamiendo de la aplicacion

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { createStackNavigator } from '@react-navigation/stack';
-
+import {Home} from '@/views'
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <Stack.Navigator>
-      {/* <Stack.Screen name="Home" options={{ headerShown: false }} component={Home} /> */}
+      <Stack.Screen name="Home" options={{ headerShown: false }} component={Home} />
     </Stack.Navigator>
   );
 }
