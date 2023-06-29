0a7382d79a38f3e8b2b9fd08912ad374ad07c0cb
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-29 17:24:29 -0500

    Creado el archivo img.ts: e importando las imagenes a utilizar en la app

---
+++ global/img.ts
@@ -0,0 +1,19 @@
+const IMGENS = Object.freeze({
+    avatar: require("../assets/images/avatar.png"),
+    beansBackground1: require("../assets/images/beansBackground1.png"),
+    beansBackground2: require("../assets/images/beansBackground2.png"),
+    coffee: require("../assets/images/coffee.png"),
+    coffee1: require("../assets/images/coffee1.png"),
+    coffee2: require("../assets/images/coffee2.png"),
+    coffee3: require("../assets/images/coffee3.png"),
+    coffee4: require("../assets/images/coffee4.png"),
+    coffee5: require("../assets/images/coffee5.png"),
+    emptyStar: require("../assets/images/emptyStar.png"),
+    line: require("../assets/images/line.png"),
+});
+const ICONS = Object.freeze({
+    apple: require("../assets/images/apple.png"),
+    faceook: require("../assets/images/facebook.png"),
+    google: require("../assets/images/google.png"),
+});
+export {ICONS,IMGENS}
