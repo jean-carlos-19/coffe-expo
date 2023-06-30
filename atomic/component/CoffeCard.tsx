ec30a1b28e538945ae484499ab93c40712b804b8
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-06-29 20:07:21 -0500

    Creado el archivo CoffeeCard.tsx

---
+++ atomic/component/CoffeCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface coffee_card_props{
+    item:any
+}
+const CoffeeCard = (props:coffee_card_props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export {CoffeeCard}
