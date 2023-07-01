fcba7d86a63f26b0e3bef3312c1a2afd58e8d163
Jean Carlos Piguave Alvarado <piguavejc@gamil.com>
2023-07-01 13:26:37 -0500

    Creado el archivo routes.ts: creaado los nombres de ruta y sus parametros

---
+++ global/routes.ts
@@ -0,0 +1,13 @@
+type RootStackParamList = {
+    Home: undefined;
+    Product: {
+        item: {
+            image: any;
+            name: string;
+            stars: string;
+            price: string;
+            volume: string;
+        };
+    };
+};
+export { RootStackParamList }
